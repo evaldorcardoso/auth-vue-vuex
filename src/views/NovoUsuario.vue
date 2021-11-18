f1281077687e2798f11bff9a63a8020975201bdf
Evaldo R. C <evaldorubel@hotmail.com>
2021-11-18 10:15:03 -0300

    P2 - Autenticando usuarios

--- src/views/NovoUsuario.vue
+++ src/views/NovoUsuario.vue
@@ -19,7 +19,6 @@
     </div>
 </template>
 <script>
-import axios from 'axios'
 
 export default {
 
@@ -34,9 +33,12 @@ export default {
   },
   methods: {
       enviarFormulario(){
-        axios
-            .post('http://localhost:8000/auth/register', this.usuario)
-            .then(response => console.log(response))
+        this.$http
+            .post('auth/register', this.usuario)
+            .then(response => {
+              console.log(response)
+              this.$router.push({ name: 'login' })
+            })
             .catch(error => console.log(error))
       }
   }
